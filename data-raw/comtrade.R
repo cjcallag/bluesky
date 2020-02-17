4bac338283cd0a9ede12382665561c2464ff29cf
Brendan Knapp <brendan.knapp@nps.edu>
2020-02-17 12:10:48 -0800

    import sf, tibble

--- data-raw/comtrade.R
+++ data-raw/comtrade.R
@@ -18,13 +18,15 @@ init <- vector(mode = "list", length = length(commod_queries))
 failures <- NULL
 
 for (i in seq_along(commod_queries)) {
-  # queries_remaining <- ct_get_remaining_hourly_queries()
-  # while (queries_remaining < 3) {
-  #   cat("\r sleeping...\n")
-  #   queries_remaining <- queries_remaining <- ct_get_remaining_hourly_queries()
-  #   Sys.sleep(1)
-  # }
-  Sys.sleep(1)
+  queries_remaining <- ct_get_remaining_hourly_queries()
+  
+  while (queries_remaining < 3) {
+    Sys.sleep(60)
+    cat("\r- sleeping...")
+    queries_remaining <- ct_get_remaining_hourly_queries()
+  }
+  
+  Sys.sleep(3)
   cat(sprintf("\r - fetching query # %d / %s ...", i, length(commod_queries)))
 
   init[[i]] <- tryCatch(
@@ -34,42 +36,15 @@ for (i in seq_along(commod_queries)) {
       commod_codes = commod_queries[[i]]
     ),
     error = function(e) {
-      message(sprintf("query # %d failed...", i))
-      message(e)
+      message(sprintf("query # %d failed...\n", i))
+      # message(e)
       failures <- c(failures, i)
       NULL
     }
   )
-  if (is.null(init[[i]])) {
-    i <- i - 1
-  }
 }
 
-
-
-
-
-# 
-# library(furrr)
-# plan(multiprocess)
-# 
-# init <- future_map(commod_queries, ~ {
-#   # while (comtradr::ct_get_remaining_hourly_queries() < 5) {
-#   #   cat("\r sleeping...")
-#   #   Sys.sleep(60)
-#   # }
-#   # Sys.sleep(1)
-#   # cat("\r fetching...")
-#   tryCatch(
-#     comtradr::ct_search(
-#       reporters = "All", partners = "Mongolia",
-#       start_date = 2015, end_date = 2019,
-#       commod_codes = .x
-#     ),
-#     error = function(e) NULL
-#   )
-# }, .progress = TRUE)
-
 comtrade <- data.table::rbindlist(init)
+
 usethis::use_data(comtrade, overwrite = TRUE)
 
